670be6afedc3fc3b62dddb0f8f9df257421aa693
Michael Dao <mike@turing.io>
2021-03-10 08:12:14 -0700

    Initial commit

---
+++ test/item_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require 'pry'
+
+
+class ItemTest < Minitest::Test
+end
---
+++ test/market_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/market'
+require './lib/vendor'
+require 'pry'
+
+class MarketTest < Minitest::Test
+end
